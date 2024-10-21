84ecb555fe3e6d43b2b974dafd4dd4629ae6af79
Yarden Ron <yardenron4@gmail.com>
2024-10-21 20:53:01 +0300

    Added ability to multi select items

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -4,25 +4,60 @@ import "./styles.scss";
 
 const Accordion = () => {
     const [selected, setSelected] = useState<string |null>(null);
+    const [isMultiSelectiionEnabled, setIsMultiSelectiionEnabled] = useState(false);
+    const [multiSelected, setMultiSelected] = useState<string[]>([]);
     
     const onTitleClick = (id: string) => {
+        if (isMultiSelectiionEnabled) {
+            handleMutliSelection(id);
+        } else {
+            handleSingleSelection(id);
+        }
+    };
+
+    const handleSingleSelection = (id: string) => {
         const newSelected = selected === id? null: id;
         setSelected(newSelected);
-    }
+    };
+
+    const handleMutliSelection = (id: string) => {
+        const multiSelectedCopy = [...multiSelected];
+        const index = multiSelected.indexOf(id);
+        if (index < 0) {
+            multiSelectedCopy.push(id);
+        } else {
+            multiSelectedCopy.splice(index, 1);
+        }
+
+        setMultiSelected(multiSelectedCopy);
+    };
+
+    const onMultiSelectionClick = () => {
+        if (isMultiSelectiionEnabled) {
+            setMultiSelected([]);
+        } else {
+            setSelected(null);
+        }
+
+        setIsMultiSelectiionEnabled(!isMultiSelectiionEnabled);
+    };
+
+    const isItemSelected = (id: string) => selected === id || multiSelected.indexOf(id) >= 0;
 
     return (
         <div className="container">
+            <button onClick={onMultiSelectionClick}>{isMultiSelectiionEnabled? "Disable Multi Selection" : "Enable Multi Selection"}</button>
             <div className="accordion">
                 {
                     data && data.length > 0? (
                         data.map(dataItem => (
                             <div className="item" key={dataItem.id}>
                                 <div className="title" onClick={() => onTitleClick(dataItem.id)}>
-                                    <span className={selected === dataItem.id? "bottom-arrow" : "right-arrow"}></span>
+                                    <span className={isItemSelected(dataItem.id)? "bottom-arrow" : "right-arrow"}></span>
                                     <h3> {dataItem.question} </h3>
                                 </div>
                                 {
-                                    selected === dataItem.id? (
+                                    isItemSelected(dataItem.id)? (
                                         <div className="content">
                                             {dataItem.answer}
                                         </div>
